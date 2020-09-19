624144d4a91bcbc9236a80551d1679782a37691f
achmadsetiadji <55737156+achmadsetiadji@users.noreply.github.com>
2020-09-19 23:37:59 +0700

    second commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block"><img class="bg-img" src="{{ asset('image/Login_image.svg') }}" alt=""></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6" style="padding-top: 10%; padding-bottom: 6%">
                                 <div class="p-5">
                                     <div class="text-center">
